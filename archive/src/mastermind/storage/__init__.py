a7f120888c2bb48bb7aa652431dfd0fa12994468
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2024-12-22 23:33:39 +0000

    chore: delete archived code from previous version

--- archive/src/mastermind/storage/__init__.py
+++
@@ -1,6 +0,0 @@
-from mastermind.storage.persistent_cache import PersistentCacheManager
-from mastermind.storage.user_data import get_user_data_manager
-
-userdata = get_user_data_manager()
-
-__all__ = ["PersistentCacheManager", "get_user_data_manager"]
