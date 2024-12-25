462fadb0f9756c73be980996ef793f04ce6b4ce5
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2024-12-25 03:51:59 +0000

    feat(lib): add library for event driven architecture and api request and response

---
+++ src/mastermind/libs/events/event_pipeline.py
@@ -0,0 +1,29 @@
+from abc import abstractmethod
+from typing import Any
+
+from mastermind.libs.events.handle_result import HandleResult
+from mastermind.libs.events.handler_pipeline import HandlerPipeline
+
+
+class BoolHandlerPipeline(HandlerPipeline):
+    """Collection of a series of bool handlers that will be called in order to handle an event. Each handler return a bool indicating whether the event was handled successfully.
+
+    If a handler returns False, the next handler will not be called and a HandleResult with handled=False will be returned. Otherwise, the next handler will be called, and a HandleResult with handled=True will be returned if all handlers return True.
+    """
+
+    def __new__(cls, **kwargs: dict[Any, Any]) -> HandleResult:
+        instance = object.__new__(cls)
+        instance.__dict__.update(kwargs)
+
+        return next(
+            (
+                HandleResult(handled=False, result=instance._package_result())
+                for handler in cls.handlers
+                if not handler(instance)  # handler expected to return a bool
+            ),
+            HandleResult(handled=True, result=instance._package_result()),
+        )
+
+    @abstractmethod
+    def _package_result(self) -> Any:
+        """Package the result of the event handling into a single object."""
---
+++ src/mastermind/libs/events/handler_pipeline.py
@@ -0,0 +1,46 @@
+from abc import abstractmethod
+from typing import Any, Callable
+
+from mastermind.libs.events.handle_result import HandleResult
+
+InstanceMethod = Callable[["HandlerPipeline"], Any]
+
+
+class HandlerPipeline:
+    def __init_subclass__(cls) -> None:
+        """Add all methods to the handlers list in the order they were defined.
+
+        This is done to ensure that the handlers are called in the order they were defined.
+        """
+
+        super().__init_subclass__()
+        cls.handlers: list[InstanceMethod] = [
+            func
+            for func in cls.__dict__.values()
+            if callable(func) and not func.__name__.startswith("_")
+        ]
+
+    @classmethod
+    def _add_handler_after(
+        cls,
+        handler: InstanceMethod,
+        after: InstanceMethod,
+    ) -> None:
+        """Add a handler to the class after another handler.
+
+        Args:
+            handler (callable): The handler function to add (should take self as the first argument).
+            after (callable): The handler function in the class that goes before the new handler.
+
+        Raises:
+            ValueError: If the *after* handler is not found in the class.
+        """
+        if after not in cls.handlers:
+            raise ValueError(f"Handler {after} not found in {cls.__name__}")
+
+        setattr(cls, handler.__name__, handler)
+        cls.handlers.insert(cls.handlers.index(after) + 1, handler)
+
+    @abstractmethod
+    def __new__(cls, **kwargs: dict[Any, Any]) -> HandleResult:
+        """Handle the event."""
