eced1fb9d2ac1ce485b3c486d06787462ffd7fa3
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2024-12-27 00:44:51 +0000

    fix(log): logger would not work when log file doesn't initially exist

--- src/mastermind/libs/logs/logger.py
+++ src/mastermind/libs/logs/logger.py
@@ -1,5 +1,6 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
 
 class Logger:
@@ -7,18 +8,7 @@ class Logger:
         self.log_file_path = log_file_path
         self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(logging.DEBUG)
-        self.create_file_handler()
-
-    def create_file_handler(self):
-        file_handler = RotatingFileHandler(
-            self.log_file_path, maxBytes=1024 * 1024 * 10, backupCount=10
-        )
-        file_formatter = logging.Formatter(
-            "%(asctime)s [%(levelname)s] %(name)s - %(message)s (File: %(filename)s, Line: %(lineno)d)",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        )
-        file_handler.setFormatter(file_formatter)
-        self.logger.addHandler(file_handler)
+        self.logger.addHandler(get_file_handler(log_file_path))
 
     def info(self, msg: str):
         self.logger.info(msg)
@@ -34,3 +24,26 @@ class Logger:
 
     def critical(self, msg: str):
         self.logger.critical(msg)
+
+
+def get_file_handler(log_file_path: str) -> RotatingFileHandler:
+    ensure_log_file_exists(log_file_path)
+
+    file_handler = RotatingFileHandler(
+        log_file_path, maxBytes=1024 * 1024 * 10, backupCount=10
+    )
+    file_formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)s] %(name)s - %(message)s (File: %(filename)s, Line: %(lineno)d)",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    file_handler.setFormatter(file_formatter)
+
+    return file_handler
+
+
+def ensure_log_file_exists(log_file_path: str):
+    log_file = Path(log_file_path)
+    log_file.parent.mkdir(parents=True, exist_ok=True)
+
+    if not log_file.exists():
+        log_file.touch()
