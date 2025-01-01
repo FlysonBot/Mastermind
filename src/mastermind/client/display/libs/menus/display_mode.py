8840314f3363611166a97c1efda97dd40c26b137
FlysonBot <flysonlin2@gmail.com>
2025-01-01 04:11:22 +0000

    chore: remove custom EnumMeta class and use pure Enum instead

--- src/mastermind/client/display/libs/menus/display_mode.py
+++ src/mastermind/client/display/libs/menus/display_mode.py
@@ -1,7 +1,7 @@
-from mastermind.libs.utils import EnumMeta
+from enum import Enum
 
 
-class DisplayMode(EnumMeta):
+class DisplayMode(Enum):
     """Enum for the different display modes, which determine how the menu options are displayed."""
 
     TITLE_ONLY = "TITLE_ONLY"  # for when user customized the key rendering
