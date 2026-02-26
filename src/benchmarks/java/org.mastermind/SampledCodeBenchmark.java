176b754b072d4f4daaf408be6e994afe11807a3d
FlysonBot <116744100+FlysonBot@users.noreply.github.com>
2026-02-26 02:28:22 -0800

    chore: add benchmark for SampledCode

---
+++ src/benchmarks/java/org.mastermind/SampledCodeBenchmark.java
@@ -0,0 +1,30 @@
+package org.mastermind;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 3, time = 1)
+@Fork(5)
+public class SampledCodeBenchmark {
+
+    @State(Scope.Thread)
+    public static class BenchmarkState {
+        // feedbackSize for d=9: (9+1)*(9+2)/2 = 55
+        final int sampleSize = SampledCode.calcSampleSize(Feedback.calcFeedbackSize(9));
+    }
+
+    @Benchmark
+    public void benchmarkGetSample(BenchmarkState state, Blackhole blackhole) {
+        int[] sample = SampledCode.getSample(9, 9, state.sampleSize);
+        blackhole.consume(sample);
+    }
+}
+
+/* Average Performance:
+Benchmark                                Mode  Cnt  Score   Error  Units
+SampledCodeBenchmark.benchmarkGetSample  avgt   15  9.964 ± 0.152  ms/op
+ */
